eb59633263c97e36d176651610f6671f47b5dbae
wesleydewinne <wesleydewinne@gmail.com>
2024-09-13 13:00:59 +0200

    init

---
+++ src/ApplePieRecipe.java
@@ -0,0 +1,22 @@
+public class ApplePieRecipe {
+
+public String recipe;
+
+Ingredient boter = new Ingredient("Roomboter", "gram",200.0);
+Ingredient suiker = new Ingredient("Suiker", "gram", 200.0);
+
+
+
+
+    public String stap1 = "Verwarm de oven van te voren op 170 graden Celsius (boven en onderwarmte)";
+    public String Klop het ei los en verdeel deze in twee delen. De ene helft is voor het deeg, het andere deel is voor het bestrijken van de appeltaart.;
+    public String Meng de boter, bastard suiker, zelfrijzend bakmeel, een helft van het ei, vanille suiker en een snufje zout tot een stevig deeg en verdeel deze in 3 gelijke delen.;
+    public String Schil nu de appels en snij deze in plakjes. Vermeng in een kopje de suiker en kaneel.;
+    public String Vet de springvorm in en bestrooi deze met bloem;
+    public String Gebruik een deel van het deeg om de bodem van de vorm te bedekken. Gebruik een deel van het deeg om de rand van de springvorm te bekleden. Strooi het paneermeel op de bodem van de beklede vorm. De paneermeel neemt het vocht van de appels op.;
+    public String Doe de heft van de appels in de vorm en strooi hier 1/3 van het kaneel-suiker mengsel overheen. Meng de ander helft van de appels met het overgebleven kaneel-suiker mengsel en leg deze in de vorm.";
+    public String Rol het laatste deel van de deeg uit tot een dunne lap en snij stroken van ongeveer 1 cm breed.;
+    public String Leg de stroken kuislings op de appeltaart. Met wat extra deegstroken werk je de rand rondom af. Gebruik het overgebleven ei om de bovenkant van het deeg te bestrijken.;
+    public String Zet de taart iets onder het midden van de oven. Bak de taart in 60 minuten op 170 graden Celsius (boven en onderwarmte) gaar en goudbruin.;
+
+}
---
+++ src/Ingredient.java
@@ -0,0 +1,49 @@
+public class Ingredient {
+
+    public Double amount;
+    public String name;
+    public String unit;
+
+
+    public Ingredient(String name, String unit, Double amount) {
+        this.name = name;
+        this.unit = unit;
+        this.amount = amount;
+    }
+//
+//
+//    ///////////////hoeveelheiden////////////////
+//    public class amout {
+//        public int banaan = 15;
+//        public int 75;
+//        public int 200;
+//        public int 400;
+//        public short 1
+//        public short 3;
+//        public short 8;
+//        public float 1.5f;
+//    }
+//
+//    //////////////////Eenheden//////////////////////
+//    public class unit {
+//        public String theelepels;
+//        public String snuf;
+//        public String stuk;
+//        public String gram;
+//        public String kilo;
+//    }
+//
+//    //////////////////////producten////////////////////
+//    public class name {
+//        public String ongezoutenboter;
+//        public String wittebasterdsuiker;
+//        public String zelfrijzendbakmeel;
+//        public String ei;
+//        public String vanillesuiker;
+//        public String zout;
+//        public String zoetzureappels;
+//        public String kristalsuiker;
+//        public String Kaneel;
+//        public String paneermeel;
+//    }
+}
